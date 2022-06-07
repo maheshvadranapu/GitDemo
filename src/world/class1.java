91b0163fe8c1f788f9c841f99bbcddbad9c3fe52
maheshvadranapu <maheshvadranapu6@gmail.com>
2022-06-07 12:32:38 +0530

    first commit

---
+++ src/world/class1.java
@@ -0,0 +1,16 @@
+package world;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("mahesh");
+        
+		
+	
+		
+		
+	}
+ 
+}
