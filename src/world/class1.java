dc4f24b3f3c966c73c85b01c998657906c6fe9ae
maheshvadranapu <maheshvadranapu6@gmail.com>
2022-06-09 12:18:20 +0530

    Gitx commit

--- src/world/class1.java
+++ src/world/class1.java
@@ -11,7 +11,8 @@ public class class1 {
 		System.out.println("akhil");
 		System.out.println("ravi");
 		System.out.println("vikas");
-		
+		System.out.println("Anandh");
+		System.out.println("Richa");
 	}
  
 }
